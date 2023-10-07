4400408b3b1ffafd22c8e70ef05f4d6475f97c3f
Dexy98 <davidecandia@live.it>
2023-10-07 12:41:20 +0200

    preparazione deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,23 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import Notes from "./models/Notes";
+//for deploy
+import path from "path";
 
 dotenv.config();
+
 const PORT = process.env.PORT || 5000;
+
+const basePath = path.resolve();
+
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
 
 //metodi crud
+// invia nota al database
 app.post("/notes", async (req: Request, res: Response) => {
   const newNotes = new Notes({
     title: req.body.title,
@@ -31,6 +40,12 @@ app.delete("/notes/:ID", async (req: Request, res: Response) => {
   const ID = req.params.ID;
   const note = await Notes.findByIdAndDelete(ID);
   res.json(note);
+});
+
+app.use(express.static(path.join(basePath, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(basePath, "client", "dist", "index.html"));
 });
 
 //connessione al db
